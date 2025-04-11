05565d0e6a1abfda7c53af700bf887d778261e17
gabriel20x <gabrieldev20x@gmail.com>
2025-04-11 10:48:25 -0500

    Remove Babel and PostCSS configuration files; update index.ts to export only Button component, removing Header references; delete unused CSS JSON files for Button and Header components.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { Button } from './components/Button/Button'
-import { Header } from './components/Header/Header'
 
 export type { ButtonProps } from './components/Button/Button'
-export type { HeaderProps } from './components/Header/Header'
-export { Button, Header }
-export default { Button, Header }
+export { Button }
+export default { Button }
