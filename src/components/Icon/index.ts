248374d9bdb8b4a437f0329f63da06362caba1e7
gabriel20x <gabrieldev20x@gmail.com>
2025-04-30 08:46:49 -0500

    Refactor Icon component exports and enhance IconProps interface
    - Changed IconProps interface to be exported for better accessibility.
    - Added a new index.ts file to streamline exports of various icon components.
    - Updated SVG paths in multiple icon components for consistency and accuracy.
    - Ensured proper formatting and structure in the icons file.

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,35 @@
+import {
+  ArrowBackIcon,
+  ArrowForwardIcon,
+  ChevronArrowUpIcon,
+  ChevronArrowDownIcon,
+  ExpandMoreIcon,
+  SearchIcon,
+  CheckFullIcon,
+  CheckOutlinedIcon,
+  HideIcon,
+  LockIcon,
+  WarningIcon,
+  InfoIcon,
+  FavIcon,
+  HelpFullIcon,
+  HelpOutlinedIcon
+} from './icons';
+
+export default {
+    ArrowBackIcon,
+    ArrowForwardIcon,
+    ChevronArrowUpIcon,
+    ChevronArrowDownIcon,
+    ExpandMoreIcon,
+    SearchIcon,
+    CheckFullIcon,
+    CheckOutlinedIcon,
+    HideIcon,
+    LockIcon,
+    WarningIcon,
+    InfoIcon,
+    FavIcon,
+    HelpFullIcon,
+    HelpOutlinedIcon
+  };
